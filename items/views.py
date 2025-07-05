84dfe8ca0de058c8c5f8437c6a3a09230412dc9e
Kenean Dita <KeneanSufa@gmail.com>
2025-07-05 18:10:27 +0300

    Basc browse and related item view

--- items/views.py
+++ items/views.py
@@ -3,5 +3,6 @@ from .models import Item
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=item.pk)[:4]
     
-    return render(request, 'items/detail.html', {'item': item})
+    return render(request, 'items/detail.html', {'item': item, 'related_items': related_items})
